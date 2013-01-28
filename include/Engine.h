448b2dd6d92dbc4ef26c0de1399c58e4aec2eae3
Vladislav Khorev <vladislav.khorev@fishrungames.com>
2013-01-28 06:05:00 +0000

    removing Halibut Engine

--- include/Engine.h
+++ include/Engine.h
@@ -2,20 +2,6 @@
 #define ENGINE_H_INCLUDED
 
 
-
-#ifdef TARGET_HALIBUT
-	#ifdef TARGET_WIN32
-		#define HALIBUT_WIN32
-	#endif
-
-	#ifdef TARGET_ANDROID
-		#define HALIBUT_ANDROID
-	#endif
-    #ifdef TARGET_IOS
-        #define HALIBUT_IOS
-    #endif
-#endif
-
 #ifdef TARGET_SALMON
 	#ifdef TARGET_WIN32
 		#define SALMON_WIN32
@@ -34,24 +20,12 @@
 #include "SalmonEngineWindows.h"
 #endif
 
-#ifdef HALIBUT_WIN32
-#include "HalibutEngineWindows.h"
-#endif
-
-
 #ifdef SALMON_ANDROID
 #include "SalmonEngineAndroid.h"
 #endif
-#ifdef HALIBUT_ANDROID
-#include "HalibutEngineAndroid.h"
-#endif
-
 
 #ifdef SALMON_IOS
 #include "include/SalmonEngineIos.h"
-#endif
-#ifdef HALIBUT_IOS
-#include "include/HalibutEngineIos.h"
 #endif
 
 #endif //ENGINE_H_INCLUDED
